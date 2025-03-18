eaa080feddf2b317eec18c94e7036b4d1f96420c
GiancarloRamondino <giancarlo.ramondino@gmail.com>
2025-03-18 16:27:26 +0100

    [UPD] for  and  If

---
+++ script.js
@@ -0,0 +1,16 @@
+
+let Fizz = 'Fizz';
+let Buzz ='Buzz';
+
+for(let i=0; i>100;  i++){
+    if (i%3==0 && i%5==0){   //https://www.html.it/pag/15220/operatori1/
+        console.log(FizzBuzz)
+    }
+    else if(i%3==0){
+        console.log(Fizz)
+    }
+    else(i%5==0){
+        console.log(Buzz)
+    }
+}
+
