344f64a619f4da072fad26dbdc071a795a448f48
GiancarloRamondino <giancarlo.ramondino@gmail.com>
2025-03-18 17:22:55 +0100

    [UPD] Done

--- script.js
+++ script.js
@@ -3,18 +3,18 @@ let Fizz = 'Fizz';
 let Buzz ='Buzz';
 let FizzBuzz = 'FizzBuzz';
 
-for(let i=0; i>100; i++)  {
-    if (i%3==0 && i%5==0){   //https://www.html.it/pag/15220/operatori1/
-        console.log(Fizz + Buzz)
+for(let i=1; i<=100; i++)  {
+    if (i%3==0 && i%5==0){   
+        console.log(FizzBuzz)
     }
-    else if(i%3==0){
-        console.log(Fizz)
+    else if (i%3==0){
+        console.log(Fizz);
     }
-    else if(i%5==0){
-        console.log(Buzz)
+    else if (i%5==0){
+        console.log(Buzz);
     }
     else{
-        console.log(i)
+        console.log(i);
     }
 }
 
